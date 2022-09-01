b8d714e6a32eab199c94d01c7cc82717886903e8
CindyCarr <cindyparedes001@gmail.com>
2022-09-01 13:47:33 -0400

    updated code

--- src/test/java/pages/AddEmployeePage.java
+++ src/test/java/pages/AddEmployeePage.java
@@ -3,9 +3,10 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
 //import utils.CommonMethods;
 
-public class AddEmployeePage {
+public class AddEmployeePage extends CommonMethods {
 
     @FindBy(id = "firstName")
     public WebElement firstName;
